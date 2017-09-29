755059753032ee6bcc8b43ef014b50223aaf6bd8
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2017-09-29 10:00:15 -0300

    gera uma string convertendo a imagem no csv

--- gera-csv.py
+++ gera-csv.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy
-img=Image.open('mapa-al-peq.png')
+img=Image.open('mapa-al-peq-peq.png')
 imgarray=numpy.array(img)
 
 c_la = 0
@@ -8,16 +8,22 @@ c_world = 0
 c_sea = 0
 i = 0
 
-for row in imgarray:
-  for column in row:
-    for value in column:
-      if value == 0: 
-        c_la += 1
-      if value == 128: 
-        c_world += 1
-      if value == 255: 
-        c_sea += 1
-      i += 1
-      print(i)
+s = ""
 
-print("la: %s \nworld: %s \nsea: %s" % (c_la,c_world,c_sea))
+for row in imgarray:
+  temp_s = ""
+  for column in row:
+    if column[0] == 0: 
+      c_la += 1
+      temp_s += "X,"
+    elif column[0] == 128: 
+      c_world += 1
+      temp_s += "W,"
+    else:
+      c_sea += 1
+      temp_s += "·,"
+  
+  s += temp_s[:-1] + "\n"
+
+# print("la: %s \nworld: %s \nsea: %s" % (c_la,c_world,c_sea))
+print(s)
