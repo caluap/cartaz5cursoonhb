483c42cc1ac64cf063163f62f280764d70dd7de8
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2017-09-29 16:13:01 -0300

    pré-mudança de abordagem (nodebox não deixa fazer lookups dinâmicos)

--- gera-csv.py
+++ gera-csv.py
@@ -10,20 +10,31 @@ i = 0
 
 s = ""
 
+header = ""
+
 for row in imgarray:
+  if header == "":
+    for i_h in range(len(row)):
+      header += "C" + str(i_h) + ","
   temp_s = ""
   for column in row:
     if column[0] == 0: 
       c_la += 1
-      temp_s += "X,"
+      temp_s += "10,"
     elif column[0] == 128: 
       c_world += 1
-      temp_s += "W,"
+      temp_s += "4,"
     else:
       c_sea += 1
-      temp_s += "·,"
+      temp_s += "0,"
   
   s += temp_s[:-1] + "\n"
 
 # print("la: %s \nworld: %s \nsea: %s" % (c_la,c_world,c_sea))
-print(s)
+print(header[:-1])
+print(s[:-1])
+
+text_file = open("mapa.csv", "w")
+text_file.write("%s\n%s" % (header[:-1], s[:-1]))
+# text_file.write(s[:-1])
+text_file.close()
